9b7fdc8e914ff12151b104e4c3f72e36153f790e
Alexander Telkov <alexandertelkov@gmail.com>
2015-08-02 16:06:49 +0300

    update main

--- main.py
+++ main.py
@@ -1,26 +1,46 @@
 #!/usr/bin/env python
 # coding: utf-8
-__author__ = 'midiblack'
 
 import requests
 
 
+def status_code(url):
+
+    req = requests.get(url)
+
+    return req.status_code
+
+
+def pager(url):
+
+    pages = []
+    for i in range(1, 10):
+        current = ('%s?p=%s' % (url, i))
+        if status_code(current) == 200:
+            pages.append(current)
+
+    return pages
+
+
+def get_cars(url):
+
+    cars = []
+    for page in pager(url):
+        currentpage = requests.get(page).text.split('\n')
+        for line in currentpage:
+            if '<div class=\"description\">' in line:
+                cars.append('http://avito.ru%s' % line.split('"')[5])
+
+        return cars
+
 url = 'https://www.avito.ru/ryazan/avtomobili/chevrolet/lacetti/hetchbek'
+cars = get_cars(url)
 
-class Cars(object):
+d = {}
+for key in cars:
+    year = key.split('_')[2]
+    if year not in d:
+        d[year] = [key]
+    d[year] += [key]
 
-    def __init__(self, url):
-
-        self.url = url+'?p='
-
-    def page_Counter(self):
-
-        temp = requests.head(url+'?p=1')
-        return str(temp)
-
-
-c = Cars(url)
-if '200' in c.page_Counter():
-    print 'Wow yeees!'
-else:
-    print 'Nooo'
+print len(d['2008'])
