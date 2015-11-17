463be2df09ebc88ccdd20c1a6d7df6e4f2101a62
Alexander Telkov <alexandertelkov@gmail.com>
2015-11-17 13:07:49 +0300

    test

--- test.py
+++ test.py
@@ -1,11 +1,8 @@
 #!/usr/bin/env python
 # coding: utf-8
-
-
 from parse import AvitoParser
 
 
-url = 'https://www.avito.ru/ryazanskaya_oblast/avtomobili/chevrolet/lacetti'
-
+url = 'https://www.avito.ru/kasimov/avtomobili/vaz_lada'
 query = AvitoParser(url)
-print query.paginator()
+res = query.result()
